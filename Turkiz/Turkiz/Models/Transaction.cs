da334ff271d77b60f73890e9c57fba6f049fe516
yarinmeron <Yarinmeron1@gmail.com>
2020-09-10 16:13:53 +0300

    sec_commit

---
+++ Turkiz/Turkiz/Models/Transaction.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Turkiz.Model;
+
+namespace Turkiz.Models
+{
+    public class Transaction
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int TransactionId { get; set; } // primary key
+
+        public DateTime TransactionDate { get; set; }
+        public string UserName { get; set; } // Foreign key
+        public ICollection<Product> Products{ get; set; } // All transaction items
+        public float TotalCost { get; set; }
+        public bool TransactionType { get; set; } // 0 = purchase 1 = return
+        
+        
+        
+        
+        
+    }
+}
